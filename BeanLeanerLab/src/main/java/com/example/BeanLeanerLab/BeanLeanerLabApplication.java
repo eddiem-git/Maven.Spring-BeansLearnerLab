6e2267ca3b3779f3d617c27baa95e6481f715995
eddie <eddiem9657@gmail.com>
2019-12-09 12:18:01 -0500

    BeanLeaner

---
+++ BeanLeanerLab/src/main/java/com/example/BeanLeanerLab/BeanLeanerLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.BeanLeanerLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanLeanerLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanLeanerLabApplication.class, args);
+	}
+
+}
---
+++ BeanLeanerLab/src/test/java/com/example/BeanLeanerLab/BeanLeanerLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BeanLeanerLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanLeanerLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
